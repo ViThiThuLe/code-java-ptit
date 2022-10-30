703a5c96064081f0009e57be4c2debdef99f9ffb
ViThiThuLe <111132697+ViThiThuLe@users.noreply.github.com>
2022-10-30 22:08:33 +0700

    Update sap_xep_thoi_gian.java

--- oop/sap_xep_thoi_gian.java
+++ oop/sap_xep_thoi_gian.java
@@ -1,33 +1,56 @@
-
 package oop;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class sap_xep_thoi_gian {
+public class sap_xep_thoi_gian{
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        for(int i = 0; i < n; i++){
-            int gio = sc.nextInt();
-            int phut = sc.nextInt();
-            int giay = sc.nextInt();
-            thoigian tg = new thoigian(gio, phut, giay);
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        ArrayList<Time> a = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            Time x = new Time(in.nextInt(), in.nextInt(), in.nextInt());
+            a.add(x);
+        }
+        Collections.sort(a);
+        for (Time x : a) {
+            System.out.println(x);
         }
     }
-}
 
-class thoigian{
-    private int gio, phut, giay;
-    
-    public thoigian(int gio, int phut, int giay){
-        this.gio = gio;
-        this.phut = phut;
-        this.giay = giay;
+    public static class Time implements Comparable<Time> {
+
+        private int gio, phut, giay;
+
+        public Time(int gio, int phut, int giay) {
+            this.gio = gio;
+            this.phut = phut;
+            this.giay = giay;
+        }
+
+        public String toString() {
+            return gio + " " + phut + " " + giay;
+        }
+
+        public int compareTo(Time o) {
+            if (this.gio > o.gio) {
+                return 1;
+            } else if (this.gio < o.gio) {
+                return -1;
+            } else {
+                if (this.phut > o.phut) {
+                    return 1;
+                } else if (this.phut < o.phut) {
+                    return -1;
+                } else {
+                    if (this.giay > o.giay) {
+                        return 1;
+                    } else if (this.giay < o.giay) {
+                        return -1;
+                    }
+                }
+            }
+            return 0;
+        }
     }
-    
-    public boolean ktra(){
-        
-    }
-    
 }
