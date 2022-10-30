28d763db21beaead96dcaf53aa2a30be79db5d6d
ViThiThuLe <111132697+ViThiThuLe@users.noreply.github.com>
2022-10-30 22:06:36 +0700

    Update tong_uoc_so_1.java

--- oop/tong_uoc_so_1.java
+++ oop/tong_uoc_so_1.java
@@ -1,60 +1,36 @@
 package oop;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class tong_uoc_so_1 {
 
-    public static boolean nto(int n) {
-        if (n < 2) {
-            return false;
+    public static long Tinh(int x) {
+        int s = 0;
+        while (x % 2 == 0) {
+            s += 2;
+            x /= 2;
         }
-        for (int i = 2; i <= (int) Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
+        for (int i = 3; i <= Math.sqrt(x); i += 2) {
+            while (x % i == 0) {
+                s += i;
+                x /= i;
             }
         }
-        return true;
-    }
-
-    public static int tong(int s) {
-        int j = 2;
-        int[] b = new int[100];
-        int k = 0;
-        while (s > 1) {
-            if (s % j == 0) {
-                s /= j;
-                b[k] = j;
-                k++;
-            } else {
-                j++;
-            }
+        if (x > 1) {
+            s += x;
         }
-        int sum = 0;
-        for (int i = 0; i < k; i++) {
-            sum += b[i];
-        }
-        return sum;
+        return s;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] a = new int[n + 1];
-        int i = 0;
-        int sum = 0;
-        while (n > 0) {
-            int s = sc.nextInt();
-            if (nto(s) == true) {
-                a[i] = s;
-                i++;
-            } else {
-                a[i] = tong(s);
-                i++;
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            if (x != 1) {
+                sum += Tinh(x);
             }
-            n--;
-        }
-        for (int j = 0; j < i; j++) {
-            sum += a[j];
         }
         System.out.println(sum);
     }
