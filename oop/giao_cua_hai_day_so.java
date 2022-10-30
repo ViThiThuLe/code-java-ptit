523ebd5a13fd393803d184e3ce163d26ef80a10c
ViThiThuLe <111132697+ViThiThuLe@users.noreply.github.com>
2022-10-30 22:05:53 +0700

    Update giao_cua_hai_day_so.java

--- oop/giao_cua_hai_day_so.java
+++ oop/giao_cua_hai_day_so.java
@@ -1,41 +1,31 @@
 package oop;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class giao_cua_hai_day_so {
-
-
-    public static void giao(int[] a, int[] b, int n, int m) {
-        Arrays.sort(a);
-        Arrays.sort(b);
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (a[i] == b[j]) {
-                    System.out.print(a[i] + " ");
-                }
-            }
-        }
-        System.out.print("\n");
-
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int[] a = new int[n + 1];
-        int[] b = new int[m + 1];
-
-        for (int i = 1; i <= n; i++) {
-            a[i] = sc.nextInt();
+        int a[] = new int[100000];
+        List<Integer> b = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            if (a[x] == 0) {
+                a[x] = 1;
+            }
         }
-        for (int i = 1; i <= m; i++) {
-            b[i] = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            if (a[x] == 1) {
+                b.add(x);
+                a[x] = 0;
+            }
         }
-
-        giao(a, b, n, m);
-
+        b.sort(Comparator.naturalOrder());
+        for (int i : b) {
+            System.out.print(i + " ");
+        }
     }
 }
