cf206c7fbafe23e4d2e61fff0d3f592f9d17c64d
ViThiThuLe <111132697+ViThiThuLe@users.noreply.github.com>
2022-11-09 01:03:38 +0700

    Update khai_bao_lop_sinh_vien.java

--- oop/khai_bao_lop_sinh_vien.java
+++ oop/khai_bao_lop_sinh_vien.java
@@ -1,39 +1,32 @@
 package oop;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
-public class khai_bao_lop_sinh_vien {
+public class khai_bao_lop_nhan_vien {
 
-    public static void main(String[] args) throws ParseException {
-        Scanner sc = new Scanner(System.in);
-        String ten = sc.nextLine();
-        String lop = sc.next();
-        String ngaysinh = sc.next();
-        Float gpa = sc.nextFloat();
-        SinhVien sv = new SinhVien("B20DCCN001", ten, lop, ngaysinh, gpa);
-        System.out.println(sv);
-        sc.close();
-    }
-}
+    static class NhanVien {
 
-class SinhVien {
+        String id = "00001", name, gen, dob, add, tax, dky;
 
-    private String ma, ten, lop, ngaysinh;
-    private Float gpa;
+        public void input() {
+            Scanner sc = new Scanner(System.in);
+            name = sc.nextLine();
+            gen = sc.nextLine();
+            dob = sc.nextLine();
+            add = sc.nextLine();
+            tax = sc.nextLine();
+            dky = sc.nextLine();
+        }
 
-    SinhVien(String ma, String ten, String lop, String ngaysinh, Float gpa) throws ParseException {
-        this.ma = ma;
-        this.ten = ten;
-        this.lop = lop;
-        this.gpa = gpa;
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
-        this.ngaysinh = sdf.format(sdf.parse(ngaysinh));
+        @Override
+        public String toString() {
+            return id + " " + name + " " + gen + " " + dob + " " + add + " " + tax + " " + dky;
+        }
     }
 
-    @Override
-    public String toString() {
-        return ma + " " + ten + " " + lop + " " + ngaysinh + " " + String.format("%.2f", gpa);
+    public static void main(String[] args) {
+        NhanVien nv = new NhanVien();
+        nv.input();
+        System.out.println(nv.toString());
     }
 }
