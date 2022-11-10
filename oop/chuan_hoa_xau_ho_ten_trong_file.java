510431a0b985d8b9f1f1694aaad5ac915e19b2e3
ViThiThuLe <111132697+ViThiThuLe@users.noreply.github.com>
2022-11-10 22:22:38 +0700

    Add files via upload

---
+++ oop/chuan_hoa_xau_ho_ten_trong_file.java
@@ -0,0 +1,38 @@
+package oop;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class chuan_hoa_xau_ho_ten_trong_file {
+
+    public static String chuanhoa(String s) {
+        s = s.trim().toLowerCase();
+        s = s.replaceAll("\\s+", " ");
+        String[] a = s.split(" ");
+        String res = "";
+        for (int i = 0; i < a.length; i++) {
+            res += String.valueOf(a[i].charAt(0)).toUpperCase() + a[i].substring(1);
+            if (i < a.length - 1) {
+                res += " ";
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("DATA.in"));
+            while (sc.hasNextLine()) {
+                String s = sc.nextLine();
+                if (s.equals("END")) {
+                    break;
+                }
+                System.out.println(chuanhoa(s));
+            }
+
+        } catch (FileNotFoundException e) {
+
+        }
+    }
+}
